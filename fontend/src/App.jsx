a064d0eb5bad1781136d2e40f075b724d069494c
ayesh jayashan <135942697+ayesh20@users.noreply.github.com>
2025-07-04 16:21:37 +0530

    initilize project

---
+++ fontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div >
+       
+        <dev className="bg-amber-700 w-[50px] h-[60px]">
+    <button>echanell</button>
+        
+      </dev>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
